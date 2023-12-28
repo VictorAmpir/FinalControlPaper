ef487167ea78831f6ffea963eb59fb5f35ebd163
VictorAmpir <140673272+VictorAmpir@users.noreply.github.com>
2023-12-28 22:35:14 +1000

    Update Program.cs
    3

--- Program.cs
+++ Program.cs
@@ -16,4 +16,24 @@ for (int i = 0; i < array.Length; i++)
     System.Console.Write($"Введите {i + 1}-е слово: ");
     array[i] = Convert.ToString(Console.ReadLine()!);
 }
-
+int sizeTwo = 0;
+for(int i = 0; i < array.Length;i++)
+{
+    if (array[i].Length <= 3)
+    {
+        sizeTwo++;
+    }
+}
+string[] arrayTwo = new string[sizeTwo];
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        arrayTwo[index] = array[i];
+        index++;
+    }
+}
+arrayPrint(array);
+System.Console.WriteLine();
+arrayPrint(arrayTwo);
