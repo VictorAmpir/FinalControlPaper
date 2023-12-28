99046464bde3f1fd1cef286b5007f2815f6a887b
VictorAmpir <140673272+VictorAmpir@users.noreply.github.com>
2023-12-28 22:28:24 +1000

    Update Program.cs
    2

--- Program.cs
+++ Program.cs
@@ -8,3 +8,12 @@ void arrayPrint(string[] array)
         else System.Console.Write(", ");
     }
 }
+System.Console.WriteLine("Введите колличество слов в массиве: ");
+int size = int.Parse(System.Console.ReadLine()!);
+string[] array = new string[size];
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"Введите {i + 1}-е слово: ");
+    array[i] = Convert.ToString(Console.ReadLine()!);
+}
+
